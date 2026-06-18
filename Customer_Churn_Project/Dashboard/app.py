c3e387b88d79871be37ddfc87483c7e6df8b5f36
mahekpandey73093-pm <mahekpandey73093@gmail.com>
2026-06-18 16:08:35 +0530

    Update app.py

--- Customer_Churn_Project/Dashboard/app.py
+++ Customer_Churn_Project/Dashboard/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import joblib
-
+import os
 # =====================================
 # PAGE CONFIG
 # =====================================
